5ba090fb67b1c5de9803e278764afb7c7b6bb901
Mike Eastes <mike.eastes@code42.com>
2018-10-01 11:50:05 -0500

    Adds checkboxes and radio buttons to `<FormControls>`.

--- src/components/FormControls/FormControls.jsx
+++ src/components/FormControls/FormControls.jsx
@@ -1,8 +1,43 @@
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Radio,
+  RadioGroup,
+} from '@material-ui/core';
 import React from 'react';
-import { Button } from '@material-ui/core';
 
 const FormControls = () => (
-    <div><Button variant="contained" color="primary">Test Button</Button></div>
+  <form>
+    <FormGroup row>
+      <Button variant="contained" color="primary">
+        Test Button
+      </Button>
+    </FormGroup>
+    <FormGroup row>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Checkboxes</FormLabel>
+        <FormControlLabel control={<Checkbox name="checkboxes" />} label="Check 1" />
+        <FormControlLabel control={<Checkbox name="checkboxes" />} label="Check 2" />
+        <FormControlLabel control={<Checkbox name="checkboxes" />} label="Check 3" />
+        <FormControlLabel control={<Checkbox name="checkboxes" />} label="Check 4" />
+      </FormControl>
+    </FormGroup>
+    <FormGroup row>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Radios</FormLabel>
+        <RadioGroup name="radios" value="1">
+          <FormControlLabel value="1" control={<Radio />} label="Radio 1" />
+          <FormControlLabel value="2" control={<Radio />} label="Radio 2" />
+          <FormControlLabel value="3" control={<Radio />} label="Radio 3" />
+          <FormControlLabel value="4" control={<Radio />} label="Radio 4" />
+        </RadioGroup>
+      </FormControl>
+    </FormGroup>
+  </form>
 );
 
 export default FormControls;
