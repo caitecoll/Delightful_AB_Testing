ce8a8d65c8ae6c80111e683a80cd00849e952b9d
Mike Eastes <mike.eastes@code42.com>
2018-10-01 10:31:40 -0500

    Moves `<App>` into its own folder.

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import FormControls from '../FormControls';
+
+const App = () => (
+  <div className="app">
+    <FormControls />
+  </div>
+);
+
+export default App;
