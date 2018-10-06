b8d3c9e0aed3f5591585fa69cbae438280c4b04f
Mike Eastes <michael.eastes@gmail.com>
2018-10-06 16:53:44 -0500

    Adds example experiment.

--- src/components/FormControls/components/Dropdown.jsx
+++ src/components/FormControls/components/Dropdown.jsx
@@ -1,11 +1,44 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import React from 'react';
+import emitter from 'react-ab-test/lib/emitter';
+import Experiment from 'react-ab-test/lib/Experiment';
+import Variant from 'react-ab-test/lib/Variant';
 import styles from './Dropdown.module.scss';
+
+const starWarsMovies = [
+  'The Phantom Menace',
+  'Attack of the Clones',
+  'Revenge of the Sith',
+  'A New Hope',
+  'The Empire Strikes Back',
+  'Return of the Jedi',
+  'The Force Awakens',
+  'The Last Jedi',
+];
+
+const harryPotterMovies = [
+  "Harry Potter and the Philosopher's Stone",
+  'Harry Potter and the Chamber of Secrets',
+  'Harry Potter and the Prisoner of Azkaban',
+  'Harry Potter and the Goblet of Fire',
+  'Harry Potter and the Order of the Phoenix',
+  'Harry Potter and the Half-Blood Prince',
+  'Harry Potter and the Deathly Hallows – Part 1',
+  'Harry Potter and the Deathly Hallows – Part 2',
+];
 
 export default class RadioButtons extends React.Component {
   state = {
     value: 4,
   };
+
+  constructor(props) {
+    super(props);
+
+    emitter.addPlayListener((experimentName, variantName) => {
+      console.log(`Displaying experiment "${experimentName}"; variant "${variantName}"`);
+    });
+  }
 
   handleChange = (e) => {
     this.setState({ value: e.target.value });
@@ -15,24 +48,38 @@ export default class RadioButtons extends React.Component {
     const { value } = this.state;
     return (
       <FormControl className={styles.formControl}>
-        <InputLabel htmlFor="dropdown">Favorite Star Wars Movie?</InputLabel>
-        <Select
-          value={value}
-          onChange={this.handleChange}
-          inputProps={{
-            name: 'dropdown',
-            id: 'dropdown',
-          }}
-        >
-          <MenuItem value={1}>The Phantom Menace</MenuItem>
-          <MenuItem value={2}>Attack of the Clones</MenuItem>
-          <MenuItem value={3}>Revenge of the Sith</MenuItem>
-          <MenuItem value={4}>A New Hope</MenuItem>
-          <MenuItem value={5}>The Empire Strikes Back</MenuItem>
-          <MenuItem value={6}>Return of the Jedi</MenuItem>
-          <MenuItem value={7}>The Force Awakens</MenuItem>
-          <MenuItem value={8}>The Last Jedi</MenuItem>
-        </Select>
+        <Experiment name="Favorite Movie">
+          <Variant name="Star Wars">
+            <InputLabel htmlFor="dropdown">Favorite Star Wars Movie?</InputLabel>
+            <Select
+              value={value}
+              onChange={this.handleChange}
+              inputProps={{
+                name: 'dropdown',
+                id: 'dropdown',
+              }}
+            >
+              {starWarsMovies.map((title, index) => (
+                <MenuItem value={index + 1}>{title}</MenuItem>
+              ))}
+            </Select>
+          </Variant>
+          <Variant name="Harry Potter">
+            <InputLabel htmlFor="dropdown">Favorite Harry Potter Movie?</InputLabel>
+            <Select
+              value={value}
+              onChange={this.handleChange}
+              inputProps={{
+                name: 'dropdown',
+                id: 'dropdown',
+              }}
+            >
+              {harryPotterMovies.map((title, index) => (
+                <MenuItem value={index + 1}>{title}</MenuItem>
+              ))}
+            </Select>
+          </Variant>
+        </Experiment>
       </FormControl>
     );
   }
