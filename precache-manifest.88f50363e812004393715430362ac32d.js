ba5d6a0b41a17d89b258c0e7b88aa31495b51532
Caitlin Collins <caitecoll@gmail.com>
2018-10-16 20:47:10 -0500

    Updates

---
+++ precache-manifest.88f50363e812004393715430362ac32d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "126c224fd0b9769076f4",
+    "url": "/delightful_ab_testing/static/js/runtime~main.126c224f.js"
+  },
+  {
+    "revision": "ca56358bd23d8877a420",
+    "url": "/delightful_ab_testing/static/js/main.ca56358b.chunk.js"
+  },
+  {
+    "revision": "6788020b75438dad93e1",
+    "url": "/delightful_ab_testing/static/js/1.6788020b.chunk.js"
+  },
+  {
+    "revision": "ca56358bd23d8877a420",
+    "url": "/delightful_ab_testing/static/css/main.35143faf.chunk.css"
+  },
+  {
+    "revision": "061a22a6ee76fbc969baa6a87567a848",
+    "url": "/delightful_ab_testing/index.html"
+  }
+];
